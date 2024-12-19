381e7aba32b96a81d74d016bd042d8fa3e5db03e
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-19 13:32:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+/**
+   This program demonstrates the order in which
+   superclass and subclass constructors are called.
+*/
+
+public class Main
+{
+   public static void main(String[] args)
+   {
+      SubClass1 obj = new SubClass1();
+   }
+}
